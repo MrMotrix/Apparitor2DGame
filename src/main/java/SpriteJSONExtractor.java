158965964fd90c6c364b80add1dfdb5026c79243
RayanYI <rayanyyousfi@gmail.com>
2025-02-02 02:09:07 +0100

    add Jackson library to the project and SpriteJSONExtractor class.

---
+++ src/main/java/SpriteJSONExtractor.java
@@ -0,0 +1,40 @@
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import javax.imageio.ImageIO;
+
+public class SpriteJSONExtractor {
+
+    public static Map<String, BufferedImage> getSprites(String jsonFilePath, String spriteSheetFilePath) throws IOException {
+
+        Map<String, Map<String, Object>> jsonMap = null;
+        BufferedImage spriteSheet = null;
+        File JSON_SOURCE = new File(jsonFilePath);
+
+        try {
+            jsonMap = new ObjectMapper().readValue(JSON_SOURCE, HashMap.class);
+            spriteSheet = ImageIO.read(new File(spriteSheetFilePath));
+        } catch (Exception e) {
+            throw new IOException(e.getMessage());
+        }
+
+        Map<String, BufferedImage> sprites = new HashMap<>();
+
+        for (String key : jsonMap.keySet()) {
+            Map<String, Object> spriteInfo = (Map<String, Object>) jsonMap.get(key).get("sprite");
+
+            int x = (int) spriteInfo.get("x");
+            int y = (int) spriteInfo.get("y");
+            int w = (int) spriteInfo.get("w");
+            int h = (int) spriteInfo.get("h");
+
+            BufferedImage sprite = spriteSheet.getSubimage(x, y, w, h);
+            sprites.put(key, sprite);
+        }
+
+        return sprites;
+    }
+}
