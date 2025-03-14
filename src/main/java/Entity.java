46ac565be57c2e6396099d8f2704efcbed92e4ec
RayanYI <rayanyyousfi@gmail.com>
2025-03-14 14:33:29 +0100

    refactor(entity): now more abstract

--- src/main/java/Entity.java
+++ src/main/java/Entity.java
@@ -1,32 +1,13 @@
+import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
+import java.util.List;
 
+public abstract class Entity {
+    public Vec2 position;
 
-public class Entity {
-    protected Vec2 position;
-    protected int speed;
-    protected String direction;
-    protected int spriteCounter;
-    protected int spriteNum;
-
-    public Entity() {
-        position = new Vec2(0, 0);
-        direction = "down";
-        spriteCounter = 0;
-        spriteNum = 1;
+    public Entity(Vec2 position) {
+        this.position = position;
     }
 
-    public Vec2 getPosition() {
-        return position;
-    }
-
-    public void setPosition(int x, int y) {
-        position.set(x, y);
-    }
-
-    public String getDirection() {
-        return direction;
-    }
-
-    public void setDirection(String dir) {
-        direction = dir;
-    }
+    public abstract void update();
 }
