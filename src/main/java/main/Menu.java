a1f3a0b03bf7dda243707e16271e12fe59c7318e
Slaytt <slaytran7@gmail.com>
2025-03-27 22:42:42 +0100

    .

--- src/main/java/main/Menu.java
+++ src/main/java/main/Menu.java
@@ -6,7 +6,6 @@ import java.awt.Graphics2D;
 
 public class Menu {
     private GamePanel gamePanel;
-    private int selectedOption = 0;
 
     public Menu(GamePanel gamePanel) {
         this.gamePanel = gamePanel;
@@ -20,16 +19,7 @@ public class Menu {
         g2.setColor(Color.WHITE);
         g2.drawString("MENU PAUSE", gamePanel.screenWidth / 2 - 80, gamePanel.screenHeight / 2 - 100);
 
-        String[] options = {"Reprendre", "Quitter"}; //on peut rajouter ce qu'on veut comme btn
-        for (int i = 0; i < options.length; i++) {
-            if (i == selectedOption) {
-                g2.setColor(Color.WHITE);
-            }
-
-            else {
-                g2.setColor(Color.WHITE);
-            }
-            g2.drawString(options[i], gamePanel.screenWidth /2-60, gamePanel.screenHeight / 2 + i * 40);
-        }
+        g2.drawString("Reprendre", gamePanel.screenWidth / 2 - 60, gamePanel.screenHeight / 2);
+        g2.drawString("Quitter", gamePanel.screenWidth / 2 - 60, gamePanel.screenHeight / 2 + 40);
     }
 }
