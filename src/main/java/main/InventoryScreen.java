56a3d674e795e5bbc7f37db6e21212ddcc666c27
RayanYI <rayanyyousfi@gmail.com>
2025-04-02 23:09:58 +0200

    fix inventory screen position

--- src/main/java/main/InventoryScreen.java
+++ src/main/java/main/InventoryScreen.java
@@ -15,8 +15,8 @@ public class InventoryScreen extends Screen {
 
 
 
-    public int MenuX = 10;
-    public int MenuY = 10;
+    public int MenuX = 30;
+    public int MenuY = 30;
 
     public InventoryScreen(GamePanel gamePanel, Inventory inventory) {
         super(gamePanel);
@@ -26,16 +26,16 @@ public class InventoryScreen extends Screen {
     @Override
     public void draw(Graphics2D g2) {
         g2.setColor(new Color(255, 255, 255, 150));
-        g2.fillRect( menuX - 320, menuY - 300, menuWidth + 20, optionHeight * (inventory.getItems().size() + 1));
+        g2.fillRect( MenuX, MenuY, menuWidth + 20, optionHeight * (inventory.getItems().size() + 1));
         g2.setColor(Color.WHITE);
         g2.setFont(new Font("Arial", Font.BOLD, 16));
-        g2.drawString("Inventaire", menuX - 270, menuY - 300);
+        g2.drawString("Inventaire", MenuX, MenuY);
 
         List<SuperObject> items = inventory.getItems();
         for (int i = 0; i < items.size(); i++) {
             SuperObject item = items.get(i);
 
-            int itemY = menuY + (i * optionHeight);
+            int itemY = MenuY + (i * optionHeight);
 
             if (i == selectedIndex) {
                 g2.setColor(Color.YELLOW);
@@ -43,12 +43,12 @@ public class InventoryScreen extends Screen {
             else {
                 g2.setColor(Color.WHITE);
             }
-            drawMenuBox(g2, i * optionHeight, menuY -300, menuX -250);
+            drawMenuBox(g2, i * optionHeight, MenuY, MenuX);
 
             if (item.getSprite() != null) {
                 g2.drawImage(
                         item.getSprite().getSprite(),
-                        menuX + PADDING +20,
+                        MenuX + PADDING +20,
                         itemY + (optionHeight - SPRITE_SIZE) / 2,
                         SPRITE_SIZE,
                         SPRITE_SIZE,
@@ -56,7 +56,7 @@ public class InventoryScreen extends Screen {
                 );
             }
 
-            drawMenuOption(g2, item.getName(), menuY -220, menuX -300);
+            drawMenuOption(g2, item.getName(), MenuX, itemY + (optionHeight ) - SPRITE_SIZE/2 );
         }
 
     }
